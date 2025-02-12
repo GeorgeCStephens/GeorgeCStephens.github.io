e8b3106b1160039ffa370fa9ff575beb2de8c09e
George Stephens <dga2601@gmail.com>
2025-02-12 17:30:21 +0000

    test changes

--- app/config.ts
+++ app/config.ts
@@ -1,10 +1,10 @@
 export const metaData = {
-  baseUrl: "https://nextfolio-template.vercel.app/",
-  title: "Nextfolio",
+  baseUrl: "https://georgestephens.co.uk",
+  title: "George",
   name: "Sirius",
   ogImage: "/opengraph-image.png",
   description:
-    "A clean, fast, and lightweight portfolio template built with Next.js, Vercel, and Tailwind CSS for optimal performance.",
+    "Test Automation Engineer @ IBM",
 };
 
 export const socialLinks = {
